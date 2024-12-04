807701894bbfb352e6eebe95b12d5aaf0e1ab2c9
Alicehhhmm <1245500390@qq.com>
2024-12-04 15:40:26 +0800

    feat:  Font definition

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ const App = () => {
     return (
         <>
             <h1 className='text-3xl text-indigo-600 font-bold underline'>Hello world!</h1>
+            <h2 className=' font-circular-web'> font-circular-web</h2>
+            <h2 className=' font-robert-medium'> font-robert-medium</h2>
+            <h2 className=' font-robert-regular'> font-robert-regular</h2>
+            <h2 className=' font-zentry'> font-zentry</h2>
+            <h2 className=' font-general'> font-general</h2>
         </>
     )
 }
